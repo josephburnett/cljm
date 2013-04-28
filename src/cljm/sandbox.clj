2d72bf1faed12d1cc59bab011b1ae47a4aa9b01b
Joseph Burnett <josephburnett79@gmail.com>
2013-04-28 10:55:21 -0700

    A freesound instrument, subby, in the sandbox.

--- src/cljm/sandbox.clj
+++ src/cljm/sandbox.clj
@@ -2,6 +2,7 @@
   (:use cljm.core)
   (:use cljm.notation)
   (:use cljm.filters)
+  (:use overtone.core)
   (:use overtone.inst.sampled-piano))
 
 (def base (staff [ C2-- C3- . G2. +A2-- C3- - ]))
@@ -15,4 +16,14 @@
       (up 7 base)
       (up 5 base)
       (phrase (repeat 2 base)))))
- 
+
+(definst subby []
+  (pan2 (play-buf 1 (load-sample (freesound-path 25649)))))
+(inst-fx! subby fx-distortion)
+(add-filter (f-inst subby) :subby)
+
+
+
+
+
+
