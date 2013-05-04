9fe05eeb70d2c19686ed65ed9802daf2f8172299
Joseph Burnett <josephburnett79@gmail.com>
2013-05-04 14:08:09 -0700

    Prototype frequency modulation instrument in the sandbox.

--- src/cljm/sandbox.clj
+++ src/cljm/sandbox.clj
@@ -3,7 +3,8 @@
   (:use cljm.notation)
   (:use cljm.filters)
   (:use overtone.core)
-  (:use overtone.inst.sampled-piano))
+  (:use overtone.inst.sampled-piano)
+  (:use overtone.examples.synthesis.fm))
 
 (def base (staff [ C2-- C3- . G2. +A2-- C3- - ]))
 (def blues-change
@@ -21,8 +22,13 @@
 (inst-fx! subby fx-distortion)
 (add-filter (f-inst subby) :subby)
 
-
-
-
-
+;; Adapted from overtone.examples.synthesis.fm
+(definst fminst [note 40 divisor 2.0 depth 1.0 out-bus 0 gate 1 attack 1 sustain 1 release 1]
+  (let [carrier (midicps note)
+        modulator (/ carrier divisor)
+        mod-env   (env-gen (lin-env attack sustain release))
+        amp-env   (env-gen (lin-env attack sustain release) :action FREE)]
+    (pan2 (* gate 0.5 amp-env
+                 (sin-osc (+ carrier
+                             (* mod-env  (* carrier depth) (sin-osc modulator))))))))
 
