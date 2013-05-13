f4ac73aa7aae8b3ee133403a8e1e0d43258dc94b
Joseph Burnett <josephburnett79@gmail.com>
2013-05-12 21:07:47 -0700

    An awesome baseline!

--- src/cljm/sandbox.clj
+++ src/cljm/sandbox.clj
@@ -23,14 +23,38 @@
 (add-filter (f-inst subby) :subby)
 
 ;; Adapted from overtone.examples.synthesis.fm
-(definst fminst [note 40 divisor 2.0 depth 1.0 out-bus 0 gate 1 attack 1 decay 1 sustain 1 release 1 level 1 curve -4]
+(definst fminst [note 40 divisor 2.0 depth 1.0 out-bus 0 gate 1 attack 0.1 decay 1 sustain 1 release 0.1 level 1 curve -4]
   (let [carrier (midicps note)
         modulator (/ carrier divisor)
-        mod-env   (env-gen (lin-env attack sustain release))
-        amp-env   (env-gen (adsr attack decay sustain release level curve) 
-                           :gate gate
-                           :action FREE)]
-    (pan2 (* 0.5 amp-env
+        env   (env-gen (adsr attack decay sustain release level curve) 
+                       :gate gate
+                       :action FREE)]
+    (pan2 (* 0.5 env
                  (sin-osc (+ carrier
-                             (* mod-env  (* carrier depth) (sin-osc modulator))))))))
+                             (* env  (* carrier depth) (sin-osc modulator))))))))
+
+;; (inst-fx! fminst fx-chorus)
+;; (inst-fx! fminst fx-echo)
+;; (inst-fx! fminst fx-distortion)
+
+(definst hat-in []
+  (pan2 (play-buf 1 (load-sample (freesound-path 109723)))))
+
+(definst hat-out[]
+  (pan2 (play-buf 1 (load-sample (freesound-path 109724)))))
+
+(definst fourty-four-pedal-hi-hat []
+  (pan2 (play-buf 1 (load-sample (freesound-path 22747)))))
+
+(definst dodgy-c-wood-block []
+  (pan2 (play-buf 1 (load-sample (freesound-path 53387)))))
+
+(definst low-tone-with-ringmod []
+  (pan2 (play-buf 1 (load-sample (freesound-path 38718)))))
+
+(def baseline (with [:attack 0 :depth 3.0] (with-inst fminst 
+  (staff [ A3- A2- A3-   - | - A3- A2--  | A2- A1- C2. . A1. . ]
+         [   -   - C2- D2- | -   - - A1- |   - - - -   ]))))
+
+;; (play (l (up 24 baseline)))
 
